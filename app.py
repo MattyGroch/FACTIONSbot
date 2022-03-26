d64829eadab3ea0d9e2ec1ff82717d2c1ce09062
Matt Grochocinski <matt.grochocinski@gmail.com>
2022-03-26 02:58:39 -0500

    basic implementation is working

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import discord
 from discord.ext import commands, tasks
 import os
+import json
 from dotenv import load_dotenv
 import logging
 import datetime
+import psycopg2
+from psycopg2 import OperationalError
 
 # setup logging
 logger = logging.getLogger('discord')
@@ -14,20 +17,86 @@ logger.addHandler(handler)
 
 load_dotenv()
 
-# load bot vars
+# load env vars
 bot_token = os.environ.get("BOT_TOKEN")
 announce_channel = os.environ.get("ANNOUNCE_CHANNEL_ID")
-
-# set up the DB
 db_URL = os.environ.get("DATABASE_URL", None)
-client.pg_con = await asyncpg.create_pool(db_URL)
+bot_admin = os.environ.get("BOT_ADMIN")
 
+# set bot
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="$",intents=intents)
 
 # define dict for roles
-roledict = open('roles.json')
+print(os.getcwd())
+Z = open('./FACTIONSbot/roles.json')
+roledict = json.load(Z)
+Z.close()
+
+# connect to db
+# conn = psycopg2.connect(db_URL, sslmode='require')
+conn = psycopg2.connect(db_URL)
+
+def execute_query(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+def get_info(connection,fid):
+    cursor = connection.cursor()
+    cursor.execute(f"""SELECT name FROM factions WHERE id = '{fid}'""")
+    name = cursor.fetchone();
+    cursor.execute(f"""SELECT exp FROM factions WHERE id = '{fid}'""")
+    exp = cursor.fetchone();
+    return name[0], exp[0]
+
+def initDB():
+    table_init = """
+    CREATE TABLE IF NOT EXISTS factions (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        exp INTEGER
+        )
+        """
+    execute_query(conn, table_init)
+    for h in roledict:
+        r_id = h["id"]
+        r_name = h["name"]
+        query = f"""
+            INSERT INTO
+                factions (id, name)
+            VALUES
+                ( '{r_id}' , '{r_name}' )
+            ON CONFLICT (id)
+            DO NOTHING;
+            UPDATE factions
+            SET exp=0
+            WHERE id='{r_id}' AND exp IS NULL;
+            """
+        execute_query(conn, query)
+
+
+def add_faction_xp(fid,amt):
+    query = f"""
+        UPDATE factions
+        SET exp = exp + {amt}
+        WHERE id = '{fid}';
+        """
+    execute_query(conn, query)
+
+def reset_faction_xp(fid):
+    query = f"""
+        UPDATE factions
+        SET exp = 0
+        WHERE id = '{fid}';
+        """
+    execute_query(conn, query)
+
 
 @bot.event
 async def on_ready():
@@ -46,9 +115,10 @@ async def on_member_update(before, after):
                         emoji = R["emoji"]
                         rolename = R["name"]
                         shortname = rolename.split(' ')[1]
+                        rolecolor = discord.Color.from_rgb(R["r"], R["g"], R["b"])
                         emb = discord.Embed(
                             title=f"{emoji} **{rolename}** have a new recruit!",
-                            color=R["color"],
+                            color=rolecolor,
                             timestamp=datetime.datetime.utcnow()
                         )
                         emb.set_thumbnail(url=R["image"])
@@ -57,5 +127,46 @@ async def on_member_update(before, after):
                         await channel.send(embed=emb)
 
 
+@bot.event
+async def on_message(msg):
+    if msg.author.bot == True:
+        return
+    if msg.channel.category_id == 822162540955566121 or not msg.channel.category_id:
+        return
+    else:
+        for x in roledict:
+            if x["id"] in [str(y.id) for y in msg.author.roles]:
+                match = x["id"]
+                if match:
+                    add_faction_xp(match,10)
+        await bot.process_commands(msg)
+
+
+@bot.command()
+@commands.has_any_role(bot_admin)
+async def resetall(ctx):
+    for x in roledict:
+        reset_faction_xp(x["id"])
+    await ctx.send("All Faction points were reset.")
+
+
+@bot.command()
+async def leaderboard(ctx):
+    list = "**Current Points:**"
+    for x in roledict:
+        emoji = x["emoji"]
+        fid = x["id"]
+        name, exp = get_info(conn,fid)
+        list += f"\n{emoji} {name} - {exp} Points"
+    await ctx.send(list)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, (commands.MissingRole, commands.MissingAnyRole)):
+        await ctx.send(f"Hey, you need to be <@&{bot_admin}> to do that!")
+
+
 if __name__ == "__main__":
+    initDB()
     bot.run(bot_token)
